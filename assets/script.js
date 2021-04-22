039b77a7bde3d2a55103a45494638c23b19ff55d
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-04-22 17:40:58 -0600

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  return;
+}
+
+//All characters user can choose
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numberCharacters = ["0123456789"];
+var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
